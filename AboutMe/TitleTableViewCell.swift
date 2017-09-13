47fce432efe90e3da937592bdb0b80c9ba1a19ea
jfredrickson <jfredric@gmail.com>
2017-09-12 19:37:42 -0700

    Initial commit. Layout 90%, no tables complete

---
+++ AboutMe/TitleTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TitleTableViewCell.swift
+//  AboutMe
+//
+//  Created by Joshua Fredrickson on 9/11/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+    //MARK: Properties
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
